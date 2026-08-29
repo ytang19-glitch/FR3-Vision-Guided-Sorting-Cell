2ca7c36967ab2cf8a20aa0fb99d82f35a6c30722
ytang19-glitch <ytang19@ualberta.ca>
2026-08-28 18:49:31 -0600

    Source Franka workspace in container startup

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
 set -e
 
 source /opt/ros/jazzy/setup.bash
+
+if [[ -f /opt/franka_ros2_ws/install/setup.bash ]]; then
+  source /opt/franka_ros2_ws/install/setup.bash
+fi
 
 if [[ -f /workspace/ros2_ws/install/setup.bash ]]; then
   source /workspace/ros2_ws/install/setup.bash
